62ad0abdfcc86f74f51c91143dcb45d709b52615
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-02-10 15:01:15 -0300

    Criação do ProductProjections

---
+++ src/main/java/com/devsuperior/uri2621/projections/ProductMinProjection.java
@@ -0,0 +1,8 @@
+package com.devsuperior.uri2621.projections;
+
+public interface ProductMinProjection {
+    
+    String getName();
+
+
+}
--- src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -3,7 +3,13 @@ package com.devsuperior.uri2621.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2621.entities.Product;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+
+
 
 }
