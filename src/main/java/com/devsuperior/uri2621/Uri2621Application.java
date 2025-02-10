1b0d5219e4c9c308972102386fb54f04399c49fd
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-02-10 15:50:32 -0300

    JPQL

--- src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -35,5 +35,17 @@ public class Uri2621Application implements CommandLineRunner {
 		}
 		System.out.println("\n\n");
 
+
+
+		List<ProductMinDTO> result2 = repository.search2(10,20,"P");
+		System.out.println("\n*** RESULTADO JSQL RAIZ");
+		for (ProductMinDTO obj : result2){
+			System.out.println(obj);
+		}
+
+
+
+
+
 	}
 }
--- src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2621.repositories;
 
+import com.devsuperior.uri2621.DTO.ProductMinDTO;
 import com.devsuperior.uri2621.projections.ProductMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -18,6 +19,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "AND providers.name LIKE CONCAT(:beginName, '%')")
     List<ProductMinProjection> search1(Integer min, Integer max, String beginName);
 
+
+
+    @Query("SELECT new com.devsuperior.uri2621.DTO.ProductMinDTO(obj.name) " +
+            "FROM Product obj " +
+            "WHERE obj.amount BETWEEN :min AND :max " +
+            "AND obj.provider.name LIKE CONCAT(:beginName, '%')")
+    List<ProductMinDTO> search2(Integer min, Integer max, String beginName);
 
 
 }
